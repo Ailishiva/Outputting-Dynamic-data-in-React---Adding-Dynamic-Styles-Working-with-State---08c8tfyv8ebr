7eeece3378b679024a2a2c80377cce92af140fbc
Aili shivaprasad <aili.shivaprasad@gmail.com>
2022-12-29 17:35:08 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+ const[name, setName] = useState("____");
+
+ let data = "";
+ const handlerchange=(e)=>{
+  data = e.target.value;
+ }
+
+ const final=()=>{
+  setName(data);
+ }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={handlerchange}></input>
+      <button id='button' onClick={final}>Click</button>
+      <p id='text'> Hello my name is {name} and I study at Newton School</p>
     </div>
   )
 }
